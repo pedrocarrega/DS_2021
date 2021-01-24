f273f2d282e08b3463f29f21b266f11d19a124f9
Pedro Carrega <pedro.carrega@outlook.com>
2021-01-24 17:37:09 +0000

    rollback and merge

--- src/core/Client.java
+++ src/core/Client.java
@@ -11,31 +11,29 @@ import com.bezirk.middleware.java.proxy.BezirkMiddleware;
 import com.bezirk.middleware.messages.Event;
 import com.bezirk.middleware.messages.EventSet;
 
-import events.AlertEvent;
-import events.ButtonEvent;
-import events.LightEvent;
-import examples.AirQualityUpdateEvent;
 import i18n.I18N;
 import i18n.Messages;
 
 public class Client {
-	
+
 	private static HashMap<String, Consumer<String>> menu;
 	private static Contacts contacts;
 	private static Scanner sc;
-	private static Bezirk bezirk;
-	
+	private static ReminderManager reminders;
+
 	public static void main(String[] args) {
 		
+		
+		reminders = new ReminderManager();
 		contacts = new Contacts();
 		inicializeMenu();
 		inicializeBezirk();
 		start();
-		
+
 	}
 
 	private static void start() {
-		
+
 		do {
 			System.out.println(printMenu());
 			int input = Integer.parseInt(sc.nextLine());
@@ -49,26 +47,31 @@ public class Client {
 		} while (true);	
 	}
 
-	public static void inicializeMenu() {
-				
+	private static void inicializeMenu() {
+		//TODO
+
 		menu = new HashMap<>();
 		sc = new Scanner(System.in);
 		menu.put(I18N.getString(Messages.ADD_CONTACT), (i) -> contacts.createContact(sc));
 		menu.put(I18N.getString(Messages.EDIT_CONTACT), (i) -> contacts.editContact(sc));
 		menu.put(I18N.getString(Messages.REMOVE_CONTACT), (i) -> contacts.removeContact(sc));
-		
+		menu.put(I18N.getString(Messages.ADD_REMINDER), (i) -> reminders.createReminder(sc));
+
 	}
 
 	private static void inicializeBezirk() {
-		
+
 		BezirkMiddleware.initialize();
-		bezirk = BezirkMiddleware.registerZirk("Device Client Zirk");
-		
+		final Bezirk bezirk = BezirkMiddleware.registerZirk("Device Client Zirk");
+
 		//TODO how could we do this?
-		final EventSet events = new EventSet(AlertEvent.class, ButtonEvent.class, LightEvent.class);
-		
+		List<Class<? extends Event>> listenEvents = new ArrayList<>();
+
+		@SuppressWarnings("unchecked")
+		final EventSet events = new EventSet((Class<? extends Event>[]) listenEvents.toArray());
+
 		events.setEventReceiver(new EventSet.EventReceiver() {
-			
+
 			@Override
 			public void receiveEvent(Event event, ZirkEndPoint sender) {
 				//TODO should be treated by aspects, unless its core event
@@ -78,16 +81,17 @@ public class Client {
 	}
 
 	private static String printMenu() {
-		
+
 		StringBuilder sb = new StringBuilder();
 		int x = 1;
-		
+
 		for (String s : menu.keySet()) {
 			sb.append(x++ + " - " + s);
 		}
 
 		return sb.toString();
 	}
+
 	
 	public static void sendButtonEvent() {
         final ButtonEvent buttonEvent = new ButtonEvent(true);
